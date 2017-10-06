af2a1e7a0d880e836b95a3e224fdbfcc9c490ca0
elayat <houda-la-bosse@live.fr>
2017-10-06 07:48:53 +0200

    fusion du php et html

--- web/index.php
+++ web/index.php
@@ -1,14 +1,527 @@
-<?php
-require_once( "..\\lib\\entity\\Article.php" );
+
+  <?php
+//require_once( "..\\lib\\entity\\Article.php" );
 
 
-$article = new Article;
+//$article = new Article;
 
 
+//var_dump( $article);
 
-
-var_dump( $article);
-
-$article->houdasettitre("mon nouveau titre",1);
-var_dump( $article);
+//$article->houdasettitre("mon nouveau titre",1);
+//var_dump( $article);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Netcole</title>
+    <!--css-->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/tether.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style_new.css">
+</head>
+
+<body>
+   <!--header--> 
+   <header>
+   
+    <nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      
+      <a class="navbar-brand" href="#">LOGO NETCOLE</a>
+      
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Cours</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Qui sommes-nous</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Actualités</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Blogs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>  
+          
+          <!--
+          <li class="nav-item">
+            <a class="nav-link" href="#">connection  s'abonner</a>
+          </li>
+          -->       
+          
+          <!--
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+          -->
+        </ul>
+      </div>
+    </nav>
+  </header>
+  
+    
+    <!--banniére-->
+   <section class="banner">
+      <div class="ban">
+        <img src="images/ban.jpg" alt="baniere netcole" />
+       </div>
+                     
+          <div class="inner-banner">            
+              <h1>Le <span>soutien scolaire</span> pour tous!</h1>
+              <h3>A partir de 2 euros par mois.</h3>
+          <button class="btn btn-custom">Tester NETCOLE</button>
+          </div>
+          
+   </section> 
+   
+    <!--plaire-->
+    <section class="container-fluid plaire">
+      
+			<h2>Ce qui va vous plaire</h2>
+			  <div class="row">
+    
+   
+      <div class="col-sm-3">
+        <div class=" bloc-bss-1">
+          <div class="icone text-center"><img src=""></div>
+       
+          <p class="texte  text-center"><strong>Site de soutien scolaire</strong> pour s’entraîner sur ordinateur et tablette du CP à la Terminale</p>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="bloc-bss-1">
+          <div class="icone text-center"><img src=""></div>
+          <p class="texte text-center"><strong>Fiches de révision, exercices interactifs, quiz, et vidéo</strong> pour donner le goût d’apprendre !</p>
+         
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="bloc-bss-1">
+          <div class="icone text-center"><img src=""></div>
+          <p class="texte  text-center"><strong>Service disponible 24h/24 et 7j/7</strong>, avec ou sans connexion Internet</p>
+          
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="bloc-bss-1">
+          <div class="icone text-center"><img src=""></div>
+          <p class="texte  text-center"><strong>Conçu par des enseignants</strong> et <strong>conforme aux programmes</strong> de l’Éducation nationale</p>
+          
+        </div>
+      </div>
+    </div>
+		
+
+        
+    </section>
+    
+    <!--formules-->
+
+     
+     <section class="container-fluid formules">
+           <h2>Nos Formules pour réussir l'école</h2>
+          <hr class="separator"> 
+          
+          <div class="row">
+           
+            <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4 formule formule1">
+             
+              <div class="prix">
+                  <sup><span>€</span></sup> 2
+              </div>
+              
+              <div class="mois">
+                  par mois
+              </div>
+              
+              <div class="titre titre_eco">
+                  <p>FORMULE ÉCO</p>
+              </div>
+              
+              <div class="description">
+                <p>
+                   12 mois d'abbonnement pour travailler en ligne vidéos, exercices, programme quotidien de travail, suivi scolaire.. 
+                </p>
+
+              </div>
+              
+              <div class="bouton">
+                 <div class="bouton-centre">
+                      <button class="btn btn-primary btn-lg">S'abonner</button>
+                </div>
+              </div>
+
+            </article>
+            
+            
+           <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4 formule formule2">
+             
+              <div class="prix">
+                  <sup><span>€</span></sup> 2
+              </div>
+              
+              <div class="mois">
+                  par mois
+              </div>
+              
+              <div class="titre titre_eco">
+                  <p>FORMULE ÉCO</p>
+              </div>
+              
+              <div class="description">
+                <p>
+                   12 mois d'abbonnement pour travailler en ligne vidéos, exercices, programme quotidien de travail, suivi scolaire.. 
+                </p>
+
+              </div>
+              
+              <div class="bouton">
+                 <div class="bouton-centre">
+                      <button class="btn btn-primary btn-lg">S'abonner</button>
+                </div>
+              </div>
+
+            </article>
+
+            
+           <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4 formule formule3">
+             
+             
+              <div class="prix">
+                  <sup><span>€</span></sup> 2
+              </div>
+              
+              <div class="mois">
+                  par mois
+              </div>
+              
+              <div class="titre titre_eco">
+                  <p> FORMULE ÉCO</p>
+              </div>
+              
+              <div class="description">
+                <p>
+                   12 mois d'abbonnement pour travailler en ligne vidéos, exercices, programme quotidien de travail, suivi scolaire.. 
+                </p>
+
+              </div>
+              
+              <div class="bouton">
+                 <div class="bouton-centre">
+                      <button class="btn btn-primary btn-lg">S'abonner</button>
+                </div>
+              </div>
+
+            </article>
+            
+            
+            
+
+            
+<!--            
+           
+            <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4 formule formule2">
+             <div class="entete"></div>
+              <h2>Formule Avec professeur</h2>
+              <p>On vous propose:</p>
+              <p>Formule Annuelle</p>
+              <p>Formule Trimestrielle</p>
+              <p>Formule Mensuelle</p>
+               <button class="btn-sample">s'abonner</button>
+            </article>
+          
+            <article class="col-xs-12 col-sm-12 col-md-4 col-lg-4 formule formule3">
+             <div class="entete"></div>
+              <h2>Formule Stage</h2>
+              <p>Tarif unique</p>
+               <button class="btn-sample">s'abonner</button> 
+            </article>
+          
+          
+-->
+      </div>
+        
+    </section>
+    
+    <!--formule2-->
+    
+
+     <!--
+       <div class="container-fluid formules">
+        <div class="row db-padding-btm db-attached">
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div class="db-wrapper">
+                    <div class="db-pricing-eleven db-bk-color-one">
+                        <div class="price">
+                            <sup>€</sup>2
+                                <small>par mois</small>
+                        </div>
+                        <div class="type">
+                            formule éco
+                        </div>
+                        <p>12 mois d'abbonnement</p> 
+                        <p>pour travailler en ligne</p>
+                        <p>vidéos, exercices, programme quotidien de travail, suivi scolaire..</p>
+                        <div class="pricing-footer">
+
+                            <a href="#" class="btn db-button-color-square btn-lg">S'abonner</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                 <div class="db-wrapper">
+                <div class="db-pricing-eleven db-bk-color-two popular">
+                    <div class="price">
+                        <sup>€</sup>4,98
+                                <small>par heure</small>
+                    </div>
+                    <div class="type">
+                        formule professeur
+                    </div>
+                   <p> Des cours en:</p> 
+                   <p>visioconférence,</p>
+                   <p>Web cam avec un professeur.</p>
+                    <div class="pricing-footer">
+
+                        <a href="#" class="btn db-button-color-square btn-lg">S'abonner</a>
+                    </div>
+                </div>
+                     </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                 <div class="db-wrapper">
+                <div class="db-pricing-eleven db-bk-color-three">
+                    <div class="price">
+                        <sup>€</sup>4,98
+                                <small>Les 10 heures</small>
+                    </div>
+                    <div class="type">
+                        formule stage
+                    </div>
+                   <p>Un stage en </p>
+                   <p>vidéoconférence</p>
+                   <p>une remise a niveau
+                   avec un professeur.</p>
+                    <div class="pricing-footer">
+
+                        <a href="#" class="btn db-button-color-square btn-lg">S'abonner</a>
+                    </div>
+                </div>
+                     </div>
+            </div>
+
+        </div>
+
+    </div>
+    
+    
+<div class="row db-padding-btm db-attached">
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                <div class="db-wrapper">
+                    <div class="db-pricing-eleven db-bk-color-one">
+                        <div class="price">
+                            <sup>$</sup>99
+                                <small>per quarter</small>
+                        </div>
+                        <div class="type">
+                            SMALL PLAN
+                        </div>
+                        <ul>
+
+                            <li><i class="glyphicon glyphicon-print"></i>30+ Accounts </li>
+                            <li><i class="glyphicon glyphicon-time"></i>150+ Projects </li>
+                            <li><i class="glyphicon glyphicon-trash"></i>Lead Required</li>
+                        </ul>
+                        <div class="pricing-footer">
+
+                            <a href="#" class="btn db-button-color-square btn-lg">BOOK ORDER</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                 <div class="db-wrapper">
+                <div class="db-pricing-eleven db-bk-color-two popular">
+                    <div class="price">
+                        <sup>$</sup>159
+                                <small>per quarter</small>
+                    </div>
+                    <div class="type">
+                        MEDIUM PLAN
+                    </div>
+                    <ul>
+
+                        <li><i class="glyphicon glyphicon-print"></i>30+ Accounts </li>
+                        <li><i class="glyphicon glyphicon-time"></i>150+ Projects </li>
+                        <li><i class="glyphicon glyphicon-trash"></i>Lead Required</li>
+                    </ul>
+                    <div class="pricing-footer">
+
+                        <a href="#" class="btn db-button-color-square btn-lg">BOOK ORDER</a>
+                    </div>
+                </div>
+                     </div>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+                 <div class="db-wrapper">
+                <div class="db-pricing-eleven db-bk-color-three">
+                    <div class="price">
+                        <sup>$</sup>799
+                                <small>per quarter</small>
+                    </div>
+                    <div class="type">
+                        ADVANCE PLAN
+                    </div>
+                    <ul>
+
+                        <li><i class="glyphicon glyphicon-print"></i>30+ Accounts </li>
+                        <li><i class="glyphicon glyphicon-time"></i>150+ Projects </li>
+                        <li><i class="glyphicon glyphicon-trash"></i>Lead Required</li>
+                    </ul>
+                    <div class="pricing-footer">
+
+                        <a href="#" class="btn db-button-color-square btn-lg">BOOK ORDER</a>
+                    </div>
+                </div>
+                     </div>
+            </div>
+
+        </div>
+        
+    -->
+    
+    <!--nous-->
+    <div class="container-fluid somme-nous">
+         <div class="qui-netcole">
+          <h2>Qui somme nous?</h2>
+          <h3>Netcole un site d'aide scolaire pédagogique et ludique</h3>
+          </div>
+          
+          <div class="row pre-netcole">
+            <div class="col-xs-12 col-sm-6 somme-nous1">
+              
+              <p>Créer en 2003 par Grima Eric et Dumas Pascal, sont objectid est de proposer un soutien scolaire pour tous, des stages de remise à niveau avec un professeur, un programme quotidien de travail scolaire en lien avec les nouveaux programes de l'éducation nationale. </p>
+              </div>
+          
+           <div class="col-xs-6 col-sm-3 card-deck somme-nous2">
+             <div class="card">
+              <img class="card-img-top" src="..." alt="Card image cap">
+               <div class="card-block">
+              <h4>Grima Eric</h4>
+              <p class="card-text">Enseignant au CNAM depuis 11 ans et webmaster depuis 18 ans.
+                 </div>
+                  <div class="card-footer">
+                  <small class="text-muted">Last updated 3 mins ago</small>
+                   </div>
+               </div>
+            </div> 
+            
+
+            <div class="col-xs-6 col-sm-3 card-deck somme-nous3">
+             <div>
+              <h2>Dumas Pascal</h2>
+              <p>Professeur des écoles depuis plus de 30 ans dans toutes les classes de la maternelle au CM2.</p>
+              </div>
+        
+            </div>
+    
+        </div>
+           <div class="container activite">
+            <div class="row">
+            
+            <article class="col-md-4 col-lg-12 col-xs-12 col-sm-12">
+              <h2>Entrainement personnalisé pour les élèves de primaire:</h2>
+              <div class="ban">
+                <img src="" alt="img1" />
+              </div>
+              
+      
+              <p> Avec Netcole, vos enfants peuvent révisez en ligne le programe scolaire et découvrent de nouvelles compétences adaptées à leur age.</p>
+            </article>
+            
+            <article class="col-md-4 col-lg-12 col-xs-12 col-sm-12">
+              <h2>Netcole s'engage !</h2>
+              
+              <div class="ban">
+                <img src="" alt="img2" />
+              </div>
+                <p> Un apprentissage en ligne par groupe de 5 élèves maximum ,Des élèves d’une même classe du CP à la sixième.Votre enfant travaillera en <a href="#">classe inversée</a>.Il pourra poursuivre son travail de manière autonome à la maison.
+                 <p> Son professeur pourra lui envoyer des missions à faire (Des devoirs).En cas de souci, il pourra écrire/vous pourrez écrire un mail à son professeur.</p>
+                <p>Grâce au suivi scolaire parental, vous pouvez autant que parent suivre sa progressient et sa motivation sur notre site.</p>
+              
+             </article>
+            
+           </div>
+               <button class="btn btn-custom">Je découvre les formules d'abonnement</button>
+           
+           </div>
+    
+          
+    <!--actualites-->
+    <section class="container-fluid actualites">
+        
+    </section>
+    
+    <!--cataloges-->
+    <section class="container-fluid catal">
+       <h2>Cataloge:</h2>
+           <ul>
+               <li><a href="#">article : 1</a></li>
+               <li><a href="#">article : 2</a></li>
+               <li><a href="#">article : 3</a></li>
+               <li><a href="#">article : 4</a></li>
+               <li><a href="#">article : 5</a></li>
+               <li><a href="#">article : 6</a></li>
+           </ul>
+        
+    </section>
+    
+    <!--blogs-->
+    <section class="container-fluid blog">
+      <h2>Bloges:</h2>
+       <p>le contenue de deux blogs</p>
+        
+    </section>
+    
+    <!--newsletter-->
+    <section class="container-fluid news">
+
+        
+    </section>
+    
+    <!--footer-->
+    <section class="footer">
+       <p>le footer</p>
+        
+    </section>
+    
+    
+    
+    
+    
+    
+    
+    
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/tether.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    
+    
+    
+  
+</body>
+</html>
